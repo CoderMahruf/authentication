4861603975bb13281c99420e3eaa847a2f52da18
mahruf60 <mahruf9060@gmail.com>
2025-03-19 16:17:34 +0600

    User Login with Username and Email

--- apps/user_account/models.py
+++ apps/user_account/models.py
@@ -21,7 +21,7 @@ class User(AbstractBaseUser,PermissionsMixin):
     objects = UserManager()
     
     def __str__(self):
-        return str(self.id)
+        return self.username
 
     USERNAME_FIELD = "username"
     REQUIRED_FIELDS = ["email"]
--- apps/user_account/urls.py
+++ apps/user_account/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('',Home.as_view(),name='home')
+    path('',Home.as_view(),name='home'),
+    path('login/',Login.as_view(),name='login'),
+    path('logout/',Logout.as_view(),name='logout'),
 ]
