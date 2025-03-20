c22f6378ec03d770a1629a2e43d10358a2e9d4da
mahruf60 <mahruf9060@gmail.com>
2025-03-20 15:52:41 +0600

    Password Reset by Email

--- apps/user_account/urls.py
+++ apps/user_account/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path 
 from .views import *
+from django.contrib.auth.views import PasswordResetCompleteView,PasswordResetDoneView
 
 urlpatterns = [
     path('',Home.as_view(),name='home'),
@@ -7,4 +8,8 @@ urlpatterns = [
     path('logout/',Logout.as_view(),name='logout'),
     path('registration/',Registration.as_view(),name='registration'),
     path('change-password/',ChangePassword.as_view(),name='change-password'),
+
+    path('password_reset/', SendEmailToResetPassword.as_view(), name='password_reset'),
+    path('reset/<uidb64>/<token>/', ResetPasswordConfirm.as_view(), name='password_reset_confirm'),
+    path('password_reset/done/', PasswordResetDoneView.as_view(template_name='user_account/password-reset-done.html'), name='password_reset_done'),
 ]
